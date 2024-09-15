ef5e6275fbb8a2f8117e44c39f44c82b621283d9
MarwaAshraf1812 <marashraf090@gmail.com>
2024-09-16 00:59:27 +0300

    struct: Add main project structure

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+
+
+
+export default function App() {
+  return (
+    <div>App</div>
+  )
+}
