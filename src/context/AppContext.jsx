c3930db595c3c881f7367f755128152a3430c5ed
MarwaAshraf1812 <marashraf090@gmail.com>
2024-10-10 12:50:36 +0300

    feature(front): add socket initialization

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,13 +1,32 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
+import { io } from 'socket.io-client';
 
 export const AppContext = createContext();
+const localHost = 'http://127.0.0.1:5000/';
 
 export const AppProvider = ({ children }) => {
   const [activeComponent, setActiveComponent] = useState('Dashboard');
 
+  const [socket, setSocket] = useState(null);
+
+  useEffect(() => {
+    // Connect tp socket
+    const socket = io(localHost);
+    setSocket(socket);
+
+    if (socket) {
+      socket.emit('joinCommunity', '203e0ff0-7077-4171-8bd8-a19e556a123c');
+    }
+
+    return () => {
+      // Disconnect from socket
+      socket.disconnect();
+    }
+  },[])
+
 
   return (
-    <AppContext.Provider value={{activeComponent, setActiveComponent}}>
+    <AppContext.Provider value={{activeComponent, setActiveComponent, socket}}>
       {children}
     </AppContext.Provider>
   )
