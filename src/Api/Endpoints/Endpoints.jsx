0281c0ca5fae3ab6af7ed30809bd7f8b782b2639
MarwaAshraf1812 <marashraf090@gmail.com>
2024-10-10 17:17:33 +0300

    merged

--- src/Api/Endpoints/Endpoints.jsx
+++ src/Api/Endpoints/Endpoints.jsx
@@ -2,7 +2,10 @@ import axios from 'axios';
 import { DOMAIN } from "../Config/config";
 
 // Admin
-
 export const createAdmin = (adminData) => {
-    return axios.post(`${DOMAIN}/admin`, adminData);
+    return axios.post(`${DOMAIN}/api/admin/`, adminData);
+};
+
+export const getAdmins = () => {
+    return axios.get(`${DOMAIN}/api/admin`);
 };
