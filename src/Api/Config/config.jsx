b8c1c4b9143406fb9c5bd1aebfcbd02a22e70ce9
MarwaAshraf1812 <marashraf090@gmail.com>
2024-10-19 22:39:01 +0300

    feat(front) adding the pagination  and loading state for pending users and pages

--- src/Api/Config/config.jsx
+++ src/Api/Config/config.jsx
@@ -1,2 +1,2 @@
-export const DOMAIN = "http://localhost:5000"
-// export const DOMAIN = "https://server-production-82fc.up.railway.app"
+// export const DOMAIN = "http://localhost:5000"
+export const DOMAIN = "https://server-production-82fc.up.railway.app"
